b14f33d0982118093a321c889fbda8f1e3604b5f
xakkep9000 <pirat-spy@yandex.ru>
2016-04-24 20:26:40 +0400

    rename Form1 to MainForm

--- WindowsClient/VirtualCardBoardClient/Program.cs
+++ WindowsClient/VirtualCardBoardClient/Program.cs
@@ -16,7 +16,7 @@ namespace VirtualCardBoardClient
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainForm());
         }
     }
 }
